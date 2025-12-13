2674423a11f4eddb619c237dd11f135150d45108
Bart Pleiter <a.t.pleiter@gmail.com>
2025-12-13 15:05:10 +0100

    feature: improved the test_gpu program a bit to add changing colors.
    Signed-off-by: Bart Pleiter <a.t.pleiter@gmail.com>

--- Software/C/bareMetal/test_gpu.c
+++ Software/C/bareMetal/test_gpu.c
@@ -3,6 +3,26 @@
 #define KERNEL_GPU_DATA_ASCII
 #include "libs/kernel/kernel.h"
 
+unsigned int color = 0;
+unsigned int framecounter = 0;
+unsigned int render = 1;
+
+void render_loop()
+{
+    gpu_write_pixel_data(160, 120, color);
+    gpu_write_pixel_data(160, 121, color);
+    gpu_write_pixel_data(161, 120, color);
+    gpu_write_pixel_data(161, 121, color);
+
+    fb_draw_circle(160, 120, 50, color + 0xF0);
+
+    fb_fill_rect(10, 10, 50, 50, color + 0xAA);
+
+    fb_draw_line(100, 100, 20, 20, color + 0x03);
+
+    fb_draw_rect(150, 150, 40, 30, color + 0x0F);
+}
+
 int main() {
     // Reset GPU VRAM
     gpu_clear_vram();
@@ -25,24 +45,24 @@ int main() {
     gpu_write_bg_tile(10, 11, 67, 5);
     gpu_write_bg_tile(11, 11, 68, 5);
 
-    gpu_write_pixel_data(160, 120, 0xFF);
-    gpu_write_pixel_data(160, 121, 0xFF);
-    gpu_write_pixel_data(161, 120, 0xFF);
-    gpu_write_pixel_data(161, 121, 0xFF);
-
-    fb_draw_circle(160, 120, 50, 0xF0);
-
-    fb_fill_rect(10, 10, 50, 50, 0xAA);
-
-    fb_draw_line(100, 100, 20, 20, 0x03);
-
-    fb_draw_rect(150, 150, 40, 30, 0x0F);
     
-    
+    while (1)
+    {
+        if (render)
+        {
+            render = 0;
+            render_loop();
+        }
+    }
+
     return 1;
 }
 
+
+
 void interrupt()
 {
-    // No need to handle interrupts
+    render = 1;
+    framecounter++;
+    color = framecounter >> 2;
 }
